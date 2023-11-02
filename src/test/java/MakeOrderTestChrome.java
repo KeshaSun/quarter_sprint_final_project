afa2270ea97f9742fe50e092270d36d27c2c8826
KeshaSun <innokturn@yandex.ru>
2023-11-02 23:36:41 +0300

    Продолжает бесить((

--- src/test/java/MakeOrderTestChrome.java
+++ src/test/java/MakeOrderTestChrome.java
@@ -8,12 +8,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
-
 import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(Parameterized.class)
-public class makeOrderTestChrome {
+public class MakeOrderTestChrome {
     private final String name;
     private final String surname;
     private final String address;
@@ -22,7 +20,7 @@ public class makeOrderTestChrome {
     private final String textFromHeader;
     private WebDriver driver;
 
-    public makeOrderTestChrome(String name, String surname, String address, String telephone,
+    public MakeOrderTestChrome(String name, String surname, String address, String telephone,
                                String data, String textFromHeader ) {
         this.name = name;
         this.surname = surname;
@@ -48,7 +46,7 @@ public class makeOrderTestChrome {
 
    }
     @Test
-    public void MakeOrderChrome() {
+    public void MakeOrderChromeTopOrderButton() {
         driver.get("https://qa-scooter.praktikum-services.ru/");
         //Создать Page Object для Main page для работы
         MainPage objMainPage = new MainPage(driver);
@@ -92,6 +90,56 @@ public class makeOrderTestChrome {
         OrderModalFourthPlacedOrder objOrderModalFourth = new OrderModalFourthPlacedOrder(driver);
         //Проверить что страница загрузилась
        // objOrderModalFourth.waitForLoadHeader();
+        //Проверить что текст совпадает Заказ оформлен
+        String headerText = objOrderModalFourth.getHeaderText();
+        MatcherAssert.assertThat(headerText, containsString(textFromHeader));
+    }
+
+    @Test
+    public void MakeOrderChromeBotOrderButton() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        //Создать Page Object для Main page для работы
+        MainPage objMainPage = new MainPage(driver);
+        //Нажать на кнопку кук
+        objMainPage.clicOnCookieButton();
+        //Нажать на кнопку "Заказать"
+        objMainPage.clicOnMakeOrderBot();
+        //Создать Page Object для OrderModalOne для работы
+        OrderModalOneForWhom objOrderModalOne = new OrderModalOneForWhom(driver);
+        //Дождаться пока загрузится страница
+        //objOrderModalOne.waitForLoadHeader();
+        //Заполнить поле "* Имя"
+        objOrderModalOne.setName(name);
+        //Заполнить полe "* Фамилия"
+        objOrderModalOne.setSurname(surname);
+        //Заполнить поле "* Адрес: куда привезти заказ"
+        objOrderModalOne.setAddress(address);
+        //Заполнить поле "* Станция метро"
+        objOrderModalOne.setMetroStation();
+        //Заполнить поле "* Телефон: на него позвонит курьер"
+        objOrderModalOne.setTelephone(telephone);
+        //Нажать на кнопку "Далее"
+        objOrderModalOne.clickOnNextButton();
+        //Создать Page Object для ModalTwoForWhom для работы
+        OrderModalTwoAboutRent objOrderModalTwo = new OrderModalTwoAboutRent(driver);
+        //Проверить что страница ModalTwoForWhom загрузилась
+        //objOrderModalTwo.waitForLoadHeader();
+        //Заполнить поле "* Когда привезти самокат"
+        objOrderModalTwo.setWhenBringScooter(data);
+        //Заполнить поле "* Срок аренды"
+        objOrderModalTwo.setRentalPeriod();
+        //Нажать на кнопку "Заказать"
+        objOrderModalTwo.clicOnMakeOrderButton();
+        //Создать Page Object для OrderModalThree для работы
+        OrderModalThreeWantToPlaceOrder objOrderModalThree = new OrderModalThreeWantToPlaceOrder(driver);
+        //Проверить что страница OrderModalThree загрузилась
+        //objOrderModalThree.waitTilWantToMakeOrder();
+        //Нажать на кнопку "Да"
+        objOrderModalThree.clicYesButton();
+        //Создать Page Object для OrderModalThree для работы
+        OrderModalFourthPlacedOrder objOrderModalFourth = new OrderModalFourthPlacedOrder(driver);
+        //Проверить что страница загрузилась
+        // objOrderModalFourth.waitForLoadHeader();
         //Проверить что текст совпадает Заказ оформлен
         String headerText = objOrderModalFourth.getHeaderText();
         MatcherAssert.assertThat(headerText, containsString(textFromHeader));
--- src/test/java/MakeOrderTestFirefox.java
+++ src/test/java/MakeOrderTestFirefox.java
@@ -1,3 +1,4 @@
+import jdk.jfr.Label;
 import org.hamcrest.MatcherAssert;
 import org.junit.After;
 import org.junit.Before;
@@ -44,7 +45,7 @@ public class MakeOrderTestFirefox {
       driver = new FirefoxDriver();
   }
     @Test
-    public void MakeOrderFirefox() {
+    public void MakeOrderFirefoxTopOrderButton() {
         // перешли на страницу тестового приложения
         driver.get("https://qa-scooter.praktikum-services.ru/");
         //Создать Page Object для Main page для работы
@@ -53,6 +54,57 @@ public class MakeOrderTestFirefox {
         objMainPage.clicOnCookieButton();
         //Нажать на кнопку "Заказать"
         objMainPage.clicOnMakeOrderTop();
+        //Создать Page Object для OrderModalOne для работы
+        OrderModalOneForWhom objOrderModalOne = new OrderModalOneForWhom(driver);
+        //Дождаться пока загрузится страница
+        objOrderModalOne.waitForLoadHeader();
+        //Заполнить поле "* Имя"
+        objOrderModalOne.setName(name);
+        //Заполнить полe "* Фамилия"
+        objOrderModalOne.setSurname(surname);
+        //Заполнить поле "* Адрес: куда привезти заказ"
+        objOrderModalOne.setAddress(address);
+        //Заполнить поле "* Станция метро"
+        objOrderModalOne.setMetroStation();
+        //Заполнить поле "* Телефон: на него позвонит курьер"
+        objOrderModalOne.setTelephone(telephone);
+        //Нажать на кнопку "Далее"
+        objOrderModalOne.clickOnNextButton();
+        //Создать Page Object для ModalTwoForWhom для работы
+        OrderModalTwoAboutRent objOrderModalTwo = new OrderModalTwoAboutRent(driver);
+        //Проверить что страница ModalTwoForWhom загрузилась
+        objOrderModalTwo.waitForLoadHeader();
+        //Заполнить поле "* Когда привезти самокат"
+        objOrderModalTwo.setWhenBringScooter(data);
+        //Заполнить поле "* Срок аренды"
+        objOrderModalTwo.setRentalPeriod();
+        //Нажать на кнопку "Заказать"
+        objOrderModalTwo.clicOnMakeOrderButton();
+        //Создать Page Object для OrderModalThree для работы
+        OrderModalThreeWantToPlaceOrder objOrderModalThree = new OrderModalThreeWantToPlaceOrder(driver);
+        //Проверить что страница OrderModalThree загрузилась
+        objOrderModalThree.waitTilWantToMakeOrder();
+        //Нажать на кнопку "Да"
+        objOrderModalThree.clicYesButton();
+        //Создать Page Object для OrderModalThree для работы
+        OrderModalFourthPlacedOrder objOrderModalFourth = new OrderModalFourthPlacedOrder(driver);
+        //Проверить что страница загрузилась
+        objOrderModalFourth.waitForLoadHeader();
+        //Проверить что текст совпадает Заказ оформлен
+        String headerText = objOrderModalFourth.getHeaderText();
+        MatcherAssert.assertThat(headerText, containsString(textFromHeader));
+    }
+
+    @Test
+    public void MakeOrderFirefoxBotOrderButton() {
+        // перешли на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        //Создать Page Object для Main page для работы
+        MainPage objMainPage = new MainPage(driver);
+        //Нажать на кнопку кук
+        objMainPage.clicOnCookieButton();
+        //Нажать на кнопку "Заказать"
+        objMainPage.clicOnMakeOrderBot();
         //Создать Page Object для OrderModalOne для работы
         OrderModalOneForWhom objOrderModalOne = new OrderModalOneForWhom(driver);
         //Дождаться пока загрузится страница
